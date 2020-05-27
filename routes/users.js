231403b84171026a8c49d5515caa893024234230
Jeff Cook <jecook2692@gmail.com>
2020-05-27 15:32:46 -0400

    adds funtionality to the users route

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const bcrypt = require('bcrypt');
+
+
+router.get('/', function (req, res, next) {
+    res.render('users.ejs');
+});
+
+router.post('/register', (req, res) => {
+    const { username, email, password } = req.body;
+    bcrypt.hash(password, 10, (err, superSecretPasswordHash) => {
+        db.Users.create({
+            username,
+            email,
+            password: superSecretPasswordHash,
+        }).then((result) => {
+            res.redirect('/');
+        });
+    });
+});
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    db.Users.findOne({ where: { username } })
+        .then(Users => {
+            bcrypt.compare(password, Users.password, (err, match) => {
+                if (match) {
+                    res.send('Logged in!');
+                } else {
+                    res.send('Incorrect Password');
+                };
+            });
+        })
+        .catch(() => {
+            res.send('username not found');
+        });
+});
+
+module.exports = router;
