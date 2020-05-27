789c7f2f4aa97f393cc9e1577fefd4808f052aeb
Jeff Cook <jecook2692@gmail.com>
2020-05-27 15:35:39 -0400

    adds login and register forms to app.js

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const albumsRouter = require('./routes/albums');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/albums', albumsRouter);
+
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -10,13 +32,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require('./models');
 
-
 app.get('/', (req, res) => {
-    db.Artist.findAll()
-        .then((results) => {
-            res.json(results)
-        })
+    res.send(`<h1>Welcome to the music repo!</h1>`)
 });
+
 
 app.get('/artists', (req, res) => {
     db.Artist.findAll()
@@ -24,6 +43,7 @@ app.get('/artists', (req, res) => {
             res.json(results)
         })
 });
+
 
 app.post('/artists', (req, res) => {
     db.Artist.create({
@@ -33,6 +53,7 @@ app.post('/artists', (req, res) => {
             res.send(result);
         })
 });
+
 
 app.put('/artists/:id', (req, res) => {
     db.Artist.update({
